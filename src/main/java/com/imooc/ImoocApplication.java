4dd5742b65daebb8e46dc4dd6f7d1a2716fb1eb1
李晨翔 <leechenxiang@163.com>
2017-12-08 21:34:40 +0800

    springboot整合定时任务以及异步任务

--- src/main/java/com/imooc/ImoocApplication.java
+++ src/main/java/com/imooc/ImoocApplication.java
@@ -4,12 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //扫描 mybatis mapper 包路径
 @MapperScan(basePackages = "com.imooc.mapper")
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages= {"com.imooc", "org.n3r.idworker"})
+//开启定时任务
+@EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class ImoocApplication {
 
 	public static void main(String[] args) {

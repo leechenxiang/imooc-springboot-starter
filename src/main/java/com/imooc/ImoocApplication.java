0fb6728b10e7191c9518ba3306acfff479faf278
李晨翔 <leechenxiang@163.com>
2018-03-26 12:56:12 +0800

    update to springboot 2.0.0.RELEASE

--- src/main/java/com/imooc/ImoocApplication.java
+++ src/main/java/com/imooc/ImoocApplication.java
@@ -1,11 +1,12 @@
 package com.imooc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 //扫描 mybatis mapper 包路径
